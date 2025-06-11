3a635cdb6e0f106370ec762c8382b5cfc33690e7
Campbell Barton <ideasman42@gmail.com>
2025-06-11 13:28:51 +1000

    Cleanup: re-format

--- cycle-at-point.el
+++ cycle-at-point.el
@@ -307,9 +307,10 @@ when the preset isn't found."
               (progn
                 (require preset-sym)
                 t)
-            (error (unless quiet
-                     (message "cycle-at-point: preset %S not found! (%S)" preset-id err))
-                   nil))
+            (error
+             (unless quiet
+               (message "cycle-at-point: preset %S not found! (%S)" preset-id err))
+             nil))
       (funcall preset-sym))))
 
 ;;;###autoload
